a2f32a2095de7bb0cdedc874b9da1ea10e45bd66
endulum <71853916+endulum@users.noreply.github.com>
2025-06-21 14:42:43 -0700

    add piping

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,8 +1,18 @@
 // a controller's responsibility is to handle incoming requests and send outgoing responses
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  UsePipes,
+} from '@nestjs/common';
 import { CatsService } from './cats.service';
-import { CreateCatDto } from './dto/create-cat';
+import { CreateCatDto, createCatSchema } from './dto/create-cat';
 import { Cat } from './interfaces/cat.interface';
+import { ZodValidationPipe } from 'src/shared/validation.pipe';
 
 @Controller('cats') // create a controller for the prefix `cats/`
 export class CatsController {
@@ -14,7 +24,19 @@ export class CatsController {
     return this.catsService.findAll();
   }
 
+  @Get(':id')
+  findOne(
+    @Param(
+      'id',
+      new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE }),
+    )
+    id: number,
+  ) {
+    return this.catsService.findOne(id);
+  }
+
   @Post()
+  @UsePipes(new ZodValidationPipe(createCatSchema))
   create(@Body() createCatDto: CreateCatDto) {
     this.catsService.create(createCatDto);
   }
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -5,7 +5,23 @@ import { Cat } from './interfaces/cat.interface';
 
 @Injectable() // attach metadata to the class
 export class CatsService {
-  private readonly cats: Cat[] = [];
+  private readonly cats: Cat[] = [
+    {
+      name: 'Lucky',
+      age: 7,
+      breed: 'Maine Coon',
+    },
+    {
+      name: 'Toasty',
+      age: 3,
+      breed: 'Bengal',
+    },
+    {
+      name: 'Bluestar',
+      age: 12,
+      breed: 'Shorthair',
+    },
+  ];
 
   create(cat: Cat) {
     this.cats.push(cat);
@@ -13,5 +29,9 @@ export class CatsService {
 
   findAll(): Cat[] {
     return this.cats;
+  }
+
+  findOne(id: number): Cat {
+    return this.cats[id];
   }
 }
