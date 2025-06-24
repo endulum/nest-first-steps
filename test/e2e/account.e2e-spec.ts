97c59ebc0fc77552e3997ccb2252947a87da9f4f
endulum <71853916+endulum@users.noreply.github.com>
2025-06-23 21:16:52 -0700

    split e2e spec

---
+++ test/e2e/account.e2e-spec.ts
@@ -0,0 +1,67 @@
+import { createTestApp } from '../helpers/app';
+import { type NestApp } from '../helpers/types';
+import { req } from '../helpers/req.helper';
+import { expectRes } from '../helpers/expectRes.helper';
+import { expectErrors } from '../helpers/expectErrors.helper';
+import { App } from 'supertest/types';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+describe('POST /account/signup (e2e)', () => {
+  let nestApp: NestApp;
+  let app: App;
+  let prisma: PrismaService;
+
+  beforeAll(async () => {
+    nestApp = await createTestApp();
+    app = nestApp.getHttpServer();
+    prisma = nestApp.get<PrismaService>(PrismaService);
+    await prisma.clear();
+  });
+
+  afterAll(async () => {
+    await nestApp.close();
+  });
+
+  const correctForm = {
+    username: 'user',
+    password: 'correct horse battery staple',
+    confirmPassword: 'correct horse battery staple',
+  };
+
+  it('400 if input errors', async () => {
+    await expectErrors({
+      app,
+      endpoint: 'POST /account/signup',
+      correctForm,
+      wrongFields: [
+        { username: '' },
+        { username: 'a' },
+        { username: Array(100).fill('A').join('') },
+        { username: '@@@@' },
+        { password: '' },
+        { password: 'horse' },
+        { confirmPassword: '' },
+        { confirmPassword: 'incorrect horse battery staple' },
+      ],
+    });
+  });
+
+  it('400 if username is not unique', async () => {
+    const existingUser = await prisma.user.create({
+      data: { username: 'admin', password: 'correct horse battery staple' },
+    });
+    const res = await req(app, 'POST /account/signup', {
+      form: { ...correctForm, username: existingUser.username },
+    });
+    expectRes(res, 400, 'Usernames must be unique. Please choose another.');
+  });
+
+  it('201 with data', async () => {
+    const res = await req(app, 'POST /account/signup', { form: correctForm });
+    expectRes(res, 201, 'Account successfully created.');
+    expect(res.body.data).toEqual({
+      id: 2,
+      username: correctForm.username,
+    });
+  });
+}); 
