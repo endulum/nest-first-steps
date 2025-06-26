b57f2770eeb6921e3e1687ea1675676a12810f87
endulum <71853916+endulum@users.noreply.github.com>
2025-06-26 10:30:19 -0700

    clarify steps of authentication guard

--- src/modules/account/auth.guard.ts
+++ src/modules/account/auth.guard.ts
@@ -8,6 +8,9 @@ import { JwtService } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
 import { Request } from 'express';
 import { PrismaService } from '../prisma/prisma.service';
+import { User } from '@prisma/client';
+
+type Payload = { id: number; username: string };
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -21,24 +24,38 @@ export class AuthGuard implements CanActivate {
     return type === 'Bearer' ? token : undefined;
   }
 
+  private async extractPayloadFromToken(
+    token: string,
+  ): Promise<Payload | null> {
+    try {
+      const payload: Payload = await this.jwtService.verifyAsync(token, {
+        secret: jwtConstants.secret,
+      });
+      return payload;
+    } catch {
+      return null;
+    }
+  }
+
+  private async findUserFromPayload(payload: Payload): Promise<User | null> {
+    return await this.prismaService.user.findUnique({
+      where: { id: payload.id },
+    });
+  }
+
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request: Request = context.switchToHttp().getRequest();
+
     const token = this.extractTokenFromHeader(request);
     if (!token) throw new UnauthorizedException();
 
-    try {
-      const payload: { id: number; username: string } =
-        await this.jwtService.verifyAsync(token, {
-          secret: jwtConstants.secret,
-        });
-      const user = await this.prismaService.user.findUnique({
-        where: { id: payload.id },
-      });
-      if (!user) throw new Error('No user for this payload found.');
-      request['user'] = user;
-    } catch {
-      throw new UnauthorizedException();
-    }
+    const payload = await this.extractPayloadFromToken(token);
+    if (!payload) throw new UnauthorizedException();
+
+    const user = await this.findUserFromPayload(payload);
+    if (!user) throw new UnauthorizedException();
+
+    request['user'] = user;
     return true;
   }
 }
